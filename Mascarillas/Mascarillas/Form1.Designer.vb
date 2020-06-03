13447fdbea1c9eb0a4aa794134daf53b3af9a6a9
Daniela <Daniela@DESKTOP-HT00A5J>
2020-06-02 23:26:42 -0500

    Entrega Mascarillas.

---
+++ Mascarillas/Mascarillas/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnSolicitar = New System.Windows.Forms.Button()
+        Me.txtDepartamentos = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnEntregar = New System.Windows.Forms.Button()
+        Me.txtMunicipios = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cmbDepartamento = New System.Windows.Forms.ComboBox()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.txtStock = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnSolicitar)
+        Me.GroupBox1.Controls.Add(Me.txtDepartamentos)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(65, 36)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.GroupBox1.Size = New System.Drawing.Size(386, 409)
+        Me.GroupBox1.TabIndex = 11
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Cantidad por Departamento"
+        '
+        'btnSolicitar
+        '
+        Me.btnSolicitar.BackColor = System.Drawing.Color.Gray
+        Me.btnSolicitar.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSolicitar.ForeColor = System.Drawing.Color.White
+        Me.btnSolicitar.Location = New System.Drawing.Point(112, 112)
+        Me.btnSolicitar.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnSolicitar.Name = "btnSolicitar"
+        Me.btnSolicitar.Size = New System.Drawing.Size(112, 34)
+        Me.btnSolicitar.TabIndex = 3
+        Me.btnSolicitar.Text = "Solicitar"
+        Me.btnSolicitar.UseVisualStyleBackColor = False
+        '
+        'txtDepartamentos
+        '
+        Me.txtDepartamentos.Location = New System.Drawing.Point(202, 45)
+        Me.txtDepartamentos.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtDepartamentos.Name = "txtDepartamentos"
+        Me.txtDepartamentos.Size = New System.Drawing.Size(148, 26)
+        Me.txtDepartamentos.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 49)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(162, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Cant. Departamentos"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnEntregar)
+        Me.GroupBox2.Controls.Add(Me.txtMunicipios)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Controls.Add(Me.cmbDepartamento)
+        Me.GroupBox2.Location = New System.Drawing.Point(485, 36)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.GroupBox2.Size = New System.Drawing.Size(460, 409)
+        Me.GroupBox2.TabIndex = 13
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Distribución"
+        '
+        'btnEntregar
+        '
+        Me.btnEntregar.BackColor = System.Drawing.Color.Gray
+        Me.btnEntregar.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEntregar.ForeColor = System.Drawing.Color.White
+        Me.btnEntregar.Location = New System.Drawing.Point(189, 205)
+        Me.btnEntregar.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnEntregar.Name = "btnEntregar"
+        Me.btnEntregar.Size = New System.Drawing.Size(112, 35)
+        Me.btnEntregar.TabIndex = 3
+        Me.btnEntregar.Text = "Entregar"
+        Me.btnEntregar.UseVisualStyleBackColor = False
+        '
+        'txtMunicipios
+        '
+        Me.txtMunicipios.Enabled = False
+        Me.txtMunicipios.Location = New System.Drawing.Point(258, 120)
+        Me.txtMunicipios.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtMunicipios.Name = "txtMunicipios"
+        Me.txtMunicipios.Size = New System.Drawing.Size(148, 26)
+        Me.txtMunicipios.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(33, 54)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(189, 20)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Cant. por Departamentos"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 125)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(125, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Cant. Municipios"
+        '
+        'cmbDepartamento
+        '
+        Me.cmbDepartamento.FormattingEnabled = True
+        Me.cmbDepartamento.Location = New System.Drawing.Point(258, 54)
+        Me.cmbDepartamento.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.cmbDepartamento.Name = "cmbDepartamento"
+        Me.cmbDepartamento.Size = New System.Drawing.Size(116, 28)
+        Me.cmbDepartamento.TabIndex = 0
+        '
+        'btnSalir
+        '
+        Me.btnSalir.BackColor = System.Drawing.Color.Gray
+        Me.btnSalir.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSalir.ForeColor = System.Drawing.Color.White
+        Me.btnSalir.Location = New System.Drawing.Point(65, 484)
+        Me.btnSalir.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(136, 45)
+        Me.btnSalir.TabIndex = 14
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = False
+        '
+        'txtStock
+        '
+        Me.txtStock.Enabled = False
+        Me.txtStock.Location = New System.Drawing.Point(753, 484)
+        Me.txtStock.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtStock.Multiline = True
+        Me.txtStock.Name = "txtStock"
+        Me.txtStock.Size = New System.Drawing.Size(148, 42)
+        Me.txtStock.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gray
+        Me.ClientSize = New System.Drawing.Size(1091, 565)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.txtStock)
+        Me.Name = "Form1"
+        Me.Text = "Entrega Mascarillas"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnSolicitar As Button
+    Friend WithEvents txtDepartamentos As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnEntregar As Button
+    Friend WithEvents txtMunicipios As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cmbDepartamento As ComboBox
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents txtStock As TextBox
+End Class
