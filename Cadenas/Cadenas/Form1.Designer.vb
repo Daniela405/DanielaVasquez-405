fdc2ef5c713c90dcd20f8635d9d292fe140330c7
Daniela <Daniela@DESKTOP-HT00A5J>
2020-06-03 15:23:22 -0500

    Cadenas

---
+++ Cadenas/Cadenas/Form1.Designer.vb
@@ -0,0 +1,262 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.txtborrar = New System.Windows.Forms.TextBox()
+        Me.txtminuscula = New System.Windows.Forms.TextBox()
+        Me.txtmayuscula = New System.Windows.Forms.TextBox()
+        Me.txtro = New System.Windows.Forms.TextBox()
+        Me.txtposicion = New System.Windows.Forms.TextBox()
+        Me.txtultimo = New System.Windows.Forms.TextBox()
+        Me.txtprimero = New System.Windows.Forms.TextBox()
+        Me.txtlongitud = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtcadena = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(357, 181)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(96, 36)
+        Me.Button2.TabIndex = 39
+        Me.Button2.Text = "Salir"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'txtborrar
+        '
+        Me.txtborrar.Location = New System.Drawing.Point(229, 309)
+        Me.txtborrar.Name = "txtborrar"
+        Me.txtborrar.Size = New System.Drawing.Size(100, 26)
+        Me.txtborrar.TabIndex = 38
+        '
+        'txtminuscula
+        '
+        Me.txtminuscula.Location = New System.Drawing.Point(229, 275)
+        Me.txtminuscula.Name = "txtminuscula"
+        Me.txtminuscula.Size = New System.Drawing.Size(100, 26)
+        Me.txtminuscula.TabIndex = 37
+        '
+        'txtmayuscula
+        '
+        Me.txtmayuscula.Location = New System.Drawing.Point(229, 243)
+        Me.txtmayuscula.Name = "txtmayuscula"
+        Me.txtmayuscula.Size = New System.Drawing.Size(100, 26)
+        Me.txtmayuscula.TabIndex = 36
+        '
+        'txtro
+        '
+        Me.txtro.Location = New System.Drawing.Point(229, 178)
+        Me.txtro.Name = "txtro"
+        Me.txtro.Size = New System.Drawing.Size(100, 26)
+        Me.txtro.TabIndex = 35
+        '
+        'txtposicion
+        '
+        Me.txtposicion.Location = New System.Drawing.Point(229, 210)
+        Me.txtposicion.Name = "txtposicion"
+        Me.txtposicion.Size = New System.Drawing.Size(100, 26)
+        Me.txtposicion.TabIndex = 34
+        '
+        'txtultimo
+        '
+        Me.txtultimo.Location = New System.Drawing.Point(229, 116)
+        Me.txtultimo.Name = "txtultimo"
+        Me.txtultimo.Size = New System.Drawing.Size(100, 26)
+        Me.txtultimo.TabIndex = 33
+        '
+        'txtprimero
+        '
+        Me.txtprimero.Location = New System.Drawing.Point(229, 148)
+        Me.txtprimero.Name = "txtprimero"
+        Me.txtprimero.Size = New System.Drawing.Size(100, 26)
+        Me.txtprimero.TabIndex = 32
+        '
+        'txtlongitud
+        '
+        Me.txtlongitud.Location = New System.Drawing.Point(229, 84)
+        Me.txtlongitud.Name = "txtlongitud"
+        Me.txtlongitud.Size = New System.Drawing.Size(100, 26)
+        Me.txtlongitud.TabIndex = 31
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(58, 311)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(122, 20)
+        Me.Label9.TabIndex = 30
+        Me.Label9.Text = "Borrar Espacios"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(58, 279)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(88, 20)
+        Me.Label8.TabIndex = 29
+        Me.Label8.Text = "Minusculas"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(58, 246)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(84, 20)
+        Me.Label7.TabIndex = 28
+        Me.Label7.Text = "Mayuscula"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(58, 217)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(156, 20)
+        Me.Label6.TabIndex = 27
+        Me.Label6.Text = "Posicion de la letra B"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(58, 181)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(77, 20)
+        Me.Label5.TabIndex = 26
+        Me.Label5.Text = "3ro al 8vo"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(58, 151)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(119, 20)
+        Me.Label4.TabIndex = 25
+        Me.Label4.Text = "Primer Caracter"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(58, 122)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(119, 20)
+        Me.Label3.TabIndex = 24
+        Me.Label3.Text = "Ultimo Caracter"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(58, 92)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(71, 20)
+        Me.Label2.TabIndex = 23
+        Me.Label2.Text = "Longitud"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(357, 139)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(96, 36)
+        Me.Button1.TabIndex = 22
+        Me.Button1.Text = "Mostrar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtcadena
+        '
+        Me.txtcadena.Location = New System.Drawing.Point(229, 52)
+        Me.txtcadena.Name = "txtcadena"
+        Me.txtcadena.Size = New System.Drawing.Size(100, 26)
+        Me.txtcadena.TabIndex = 21
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(58, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 20)
+        Me.Label1.TabIndex = 20
+        Me.Label1.Text = "Cadena"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ClientSize = New System.Drawing.Size(548, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.txtborrar)
+        Me.Controls.Add(Me.txtminuscula)
+        Me.Controls.Add(Me.txtmayuscula)
+        Me.Controls.Add(Me.txtro)
+        Me.Controls.Add(Me.txtposicion)
+        Me.Controls.Add(Me.txtultimo)
+        Me.Controls.Add(Me.txtprimero)
+        Me.Controls.Add(Me.txtlongitud)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtcadena)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Cadenas"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button2 As Button
+    Friend WithEvents txtborrar As TextBox
+    Friend WithEvents txtminuscula As TextBox
+    Friend WithEvents txtmayuscula As TextBox
+    Friend WithEvents txtro As TextBox
+    Friend WithEvents txtposicion As TextBox
+    Friend WithEvents txtultimo As TextBox
+    Friend WithEvents txtprimero As TextBox
+    Friend WithEvents txtlongitud As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtcadena As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Cadenas/Cadenas/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim cadena As String
+        cadena = txtcadena.Text
+        txtlongitud.Text = cadena.Length
+        txtultimo.Text = cadena.Substring(cadena.Length - 1, 1)
+        txtprimero.Text = cadena.Substring(0, 1)
+        txtro.Text = cadena.Substring(2, 7)
+        txtposicion.Text = cadena.IndexOf("B", 0) + 1
+
+        txtmayuscula.Text = cadena.ToUpper
+        txtminuscula.Text = cadena.ToLower
+        txtborrar.Text = cadena.Replace(" ", "")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+End Class
